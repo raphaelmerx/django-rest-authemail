855d14fb58dc09b9ade75eee216643817eb22019
Raphael Merx <raphael.merx@gmail.com>
2023-12-14 15:13:58 +0800

    OTP for signup

--- authemail/models.py
+++ authemail/models.py
@@ -1,5 +1,6 @@
 import binascii
 import os
+import random
 from django.conf import settings
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
@@ -11,38 +12,47 @@ from django.utils.translation import gettext_lazy as _
 from django.core.mail import send_mail
 
 # Make part of the model eventually, so it can be edited
-EXPIRY_PERIOD = 3    # days
+EXPIRY_PERIOD = 3  # days
 
 
 def _generate_code():
-    return binascii.hexlify(os.urandom(20)).decode('utf-8')
+    return binascii.hexlify(os.urandom(20)).decode("utf-8")
+
+
+def _generate_otp():
+    return random.randint(100000, 999999)
 
 
 class EmailUserManager(BaseUserManager):
-    def _create_user(self, email, password, is_staff, is_superuser,
-                     is_verified, **extra_fields):
+    def _create_user(
+        self, email, password, is_staff, is_superuser, is_verified, **extra_fields
+    ):
         """
         Creates and saves a User with a given email and password.
         """
         now = timezone.now()
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
         email = self.normalize_email(email)
-        user = self.model(email=email,
-                          is_staff=is_staff, is_active=True,
-                          is_superuser=is_superuser, is_verified=is_verified,
-                          last_login=now, date_joined=now, **extra_fields)
+        user = self.model(
+            email=email,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            is_verified=is_verified,
+            last_login=now,
+            date_joined=now,
+            **extra_fields
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        return self._create_user(email, password, False, False, False,
-                                 **extra_fields)
+        return self._create_user(email, password, False, False, False, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, True, True, True,
-                                 **extra_fields)
+        return self._create_user(email, password, True, True, True, **extra_fields)
 
 
 class EmailAbstractUser(AbstractBaseUser, PermissionsMixin):
@@ -52,37 +62,47 @@ class EmailAbstractUser(AbstractBaseUser, PermissionsMixin):
 
     Email and password are required. Other fields are optional.
     """
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
-    email = models.EmailField(_('email address'), max_length=255, unique=True)
+
+    first_name = models.CharField(_("first name"), max_length=30, blank=True)
+    last_name = models.CharField(_("last name"), max_length=30, blank=True)
+    email = models.EmailField(_("email address"), max_length=255, unique=True)
     is_staff = models.BooleanField(
-        _('staff status'), default=False,
-        help_text=_('Designates whether the user can log into this '
-                    'admin site.'))
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this " "admin site."),
+    )
     is_active = models.BooleanField(
-        _('active'), default=True,
-        help_text=_('Designates whether this user should be treated as '
-                    'active.  Unselect this instead of deleting accounts.'))
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as "
+            "active.  Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
     is_verified = models.BooleanField(
-        _('verified'), default=False,
-        help_text=_('Designates whether this user has completed the email '
-                    'verification process to allow login.'))
+        _("verified"),
+        default=False,
+        help_text=_(
+            "Designates whether this user has completed the email "
+            "verification process to allow login."
+        ),
+    )
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
         abstract = True
 
     def get_full_name(self):
         """
         Returns the first_name plus the last_name, with a space in between.
         """
-        full_name = '%s %s' % (self.first_name, self.last_name)
+        full_name = "%s %s" % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
@@ -101,7 +121,10 @@ class EmailAbstractUser(AbstractBaseUser, PermissionsMixin):
 
 class SignupCodeManager(models.Manager):
     def create_signup_code(self, user, ipaddr):
-        code = _generate_code()
+        code = _generate_otp()
+        # Ensure the code is unique. TODO: allow non-unique codes
+        while self.filter(code=code).exists():
+            code = _generate_otp()
         signup_code = self.create(user=user, code=code, ipaddr=ipaddr)
 
         return signup_code
@@ -141,9 +164,9 @@ class EmailChangeCodeManager(models.Manager):
 
 
 def send_multi_format_email(template_prefix, template_ctxt, target_email):
-    subject_file = 'authemail/%s_subject.txt' % template_prefix
-    txt_file = 'authemail/%s.txt' % template_prefix
-    html_file = 'authemail/%s.html' % template_prefix
+    subject_file = "authemail/%s_subject.txt" % template_prefix
+    txt_file = "authemail/%s.txt" % template_prefix
+    html_file = "authemail/%s.html" % template_prefix
 
     subject = render_to_string(subject_file).strip()
     from_email = settings.EMAIL_FROM
@@ -151,15 +174,16 @@ def send_multi_format_email(template_prefix, template_ctxt, target_email):
     bcc_email = settings.EMAIL_BCC
     text_content = render_to_string(txt_file, template_ctxt)
     html_content = render_to_string(html_file, template_ctxt)
-    msg = EmailMultiAlternatives(subject, text_content, from_email, [to],
-                                 bcc=[bcc_email])
-    msg.attach_alternative(html_content, 'text/html')
+    msg = EmailMultiAlternatives(
+        subject, text_content, from_email, [to], bcc=[bcc_email]
+    )
+    msg.attach_alternative(html_content, "text/html")
     msg.send()
 
 
 class AbstractBaseCode(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    code = models.CharField(_('code'), max_length=40, primary_key=True)
+    code = models.CharField(_("code"), max_length=40, primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -167,10 +191,10 @@ class AbstractBaseCode(models.Model):
 
     def send_email(self, prefix):
         ctxt = {
-            'email': self.user.email,
-            'first_name': self.user.first_name,
-            'last_name': self.user.last_name,
-            'code': self.code
+            "email": self.user.email,
+            "first_name": self.user.first_name,
+            "last_name": self.user.last_name,
+            "code": self.code,
         }
         send_multi_format_email(prefix, ctxt, target_email=self.user.email)
 
@@ -179,12 +203,12 @@ class AbstractBaseCode(models.Model):
 
 
 class SignupCode(AbstractBaseCode):
-    ipaddr = models.GenericIPAddressField(_('ip address'))
+    ipaddr = models.GenericIPAddressField(_("ip address"))
 
     objects = SignupCodeManager()
 
     def send_signup_email(self):
-        prefix = 'signup_email'
+        prefix = "signup_email"
         self.send_email(prefix)
 
 
@@ -192,23 +216,20 @@ class PasswordResetCode(AbstractBaseCode):
     objects = PasswordResetCodeManager()
 
     def send_password_reset_email(self):
-        prefix = 'password_reset_email'
+        prefix = "password_reset_email"
         self.send_email(prefix)
 
 
 class EmailChangeCode(AbstractBaseCode):
-    email = models.EmailField(_('email address'), max_length=255)
+    email = models.EmailField(_("email address"), max_length=255)
 
     objects = EmailChangeCodeManager()
 
     def send_email_change_emails(self):
-        prefix = 'email_change_notify_previous_email'
+        prefix = "email_change_notify_previous_email"
         self.send_email(prefix)
 
-        prefix = 'email_change_confirm_new_email'
-        ctxt = {
-            'email': self.email,
-            'code': self.code
-        }
+        prefix = "email_change_confirm_new_email"
+        ctxt = {"email": self.email, "code": self.code}
 
         send_multi_format_email(prefix, ctxt, target_email=self.email)
